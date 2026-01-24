aac3d11bb0df593cf8400944eb94ca0a65dfc43f
jploal6 <jploal6@gmail.com>
2026-01-24 01:47:53 +0100

    First Commit

---
+++ src/Circulo.java
@@ -0,0 +1,16 @@
+public class Circulo{
+    private double radio;
+
+    public Circulo(double radio){
+        if (radio < 0) this.radio = 0;
+        else this.radio = radio;
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public double getArea() {
+        return radio * radio * Math.PI;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+void main() {
+    Cilindro cilindro = new Cilindro(5, 10);
+
+    System.out.println(cilindro.getRadio());
+    System.out.println(cilindro.getAltura());
+    System.out.println(cilindro.getArea());
+    System.out.println(cilindro.getVolume());
+}
